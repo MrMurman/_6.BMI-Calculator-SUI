d2d57ad0d2543ab1881d403212c3381c7a963bac
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-02-02 21:10:07 +0300

    Initial Commit

---
+++ _6.BMI-Calculator-SUI/_6_BMI_Calculator_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_BMI_Calculator_SUIApp.swift
+//  _6.BMI-Calculator-SUI
+//
+//  Created by Андрей Бородкин on 02.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct _6_BMI_Calculator_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
